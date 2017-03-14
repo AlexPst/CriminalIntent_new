de051e150a1f650095219cb473860b4d478bdb62
154533@mail.ru <154533@mail.ru>
2017-03-14 21:50:49 +0300

    CriminalIntent(12.03.2017)

--- app/src/main/java/ru/alex_pst/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/alex_pst/criminalintent/CrimeListFragment.java
@@ -2,10 +2,13 @@ package ru.alex_pst.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static ru.alex_pst.criminalintent.R.layout.fragment_crime_list;
 
 /**
  * Created by Alex_ on 12.03.2017.
@@ -18,7 +21,8 @@ public class CrimeListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
-        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
