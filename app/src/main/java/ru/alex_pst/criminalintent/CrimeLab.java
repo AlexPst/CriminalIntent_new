68621b42eedff6c2af79769370d5b623dfdc8fb8
154533@mail.ru <154533@mail.ru>
2017-03-19 17:02:48 +0300

    CriminalIntent(19.03.2017)

--- app/src/main/java/ru/alex_pst/criminalintent/CrimeLab.java
+++ app/src/main/java/ru/alex_pst/criminalintent/CrimeLab.java
@@ -16,29 +16,34 @@ public class CrimeLab {
     private List<Crime> mCrimes;
 
 
-    private static CrimeLab get(Context context){
-        if (sCrimeLab == null){
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
+
     private CrimeLab(Context context) {
         //mAppContext = appContext;
         mCrimes = new ArrayList<>();
-        for (int i = 0; i<100; i++){
+        for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
-            crime.setSolved(i%2 == 0);
+            crime.setSolved(i % 2 == 0);
             mCrimes.add(crime);
         }
 
     }
-    public Crime getCrimes(UUID id){
-        for (Crime crime: mCrimes){
-            if (crime.getId().equals(id)){
+
+    public Crime getCrimes(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
                 return crime;
             }
         }
         return null;
     }
+
+
 }
+    
